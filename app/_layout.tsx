5e3c0aeb6f3c3acd95fef29a836cccc34c4c695a
Milton Munoz <livemilton@gmail.com>
2026-09-23 21:44:54 -0500

    feat((HU-05)-HU(HU-05)): :sparkles: pushNotifications App - React Native - init project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,35 +3,33 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const backckgroundColor = useThemeColor({}, 'background')
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack screenOptions={{
+        headerShadowVisible: false,
+        contentStyle: { backgroundColor }
+      }}>
+        <Stack.Screen name="index" options={{ title: 'Push App' }} />
       </Stack>
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import { ThemedText } from '@/components/ThemedText';
+import React from 'react'
+
+const PushApp = () => {
+    return (
+        <View>
+            <ThemedText>PushApp</ThemedText>
+        </View>
+    );
+};
+
+export default PushApp
